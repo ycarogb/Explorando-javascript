bb3fbe195e453d0a1e2bbea7805f7488bd0f3e59
Ycaro Batalha <ycarobatalha1@gmail.com>
2024-09-10 21:31:16 -0300

    Aula sobre laços em javascript

---
+++ Lacos em javascript.js
@@ -0,0 +1,33 @@
+////// ESTRUTURAS INICIAIS ////////////
+
+/* 
+let c = 1;
+console.log('Tudo bem?');
+ 
+//Estrutura de repetição em JS
+//Estrutura de repetição com teste lógico no início:
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++
+}
+
+//Estrutura de repetição com teste lógico no final:
+
+do {
+    console.log(`Passo ${c}`);
+    c++
+} while (c <= 6)
+
+*/
+
+/////// REPETIÇÃO COM FOR /////////
+
+for(let x = 1; x <=10; x++) {
+    console.log(`repetição ${x}`)
+}
+
+
+
+
+
+
